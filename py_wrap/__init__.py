a232d74c77af970058fade984c2d81697fec543a
raphidoc <raphael.mabit@gmail.com>
2024-06-25 17:40:03 -0400

    code template for input params parsing

--- py_wrap/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-package_dir = os.path.dirname(__file__)
-rel_path = os.path.join('..', '6sV2.1', 'sixsV2.1')
-abs_path = os.path.abspath(os.path.join(package_dir, rel_path))
-
-sixs_path = abs_path
